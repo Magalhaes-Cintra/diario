3d0693fe970643f472dbfa150764b6e23d10d356
Magalhaes-Cintra <132522385+Magalhaes-Cintra@users.noreply.github.com>
2024-03-11 22:54:03 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCxvlE20bf-JNRxZpRVTuDWM8046JlJxMw",
+  authDomain: "diary-5d22d.firebaseapp.com",
+  projectId: "diary-5d22d",
+  storageBucket: "diary-5d22d.appspot.com",
+  messagingSenderId: "101454321476",
+  appId: "1:101454321476:web:710cf7c472e781be452911"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
